6970315d80d44f6fed86338e346d05955478a2a7
andehall <anders.hallgren@live.se>
2015-01-02 14:39:28 +0100

    implemented IGuestsTest method

--- Hotel.tests/src/Classes/Guests/tests/IGuestsTest.java
+++ Hotel.tests/src/Classes/Guests/tests/IGuestsTest.java
@@ -30,9 +30,9 @@ public class IGuestsTest {
 	
 	@After
 	public void tearDown() throws Exception {
-		IGuests.INSTANCE.removeGuest("010101-0101");
-		IGuests.INSTANCE.removeGuest("020202-0202");
-		IGuests.INSTANCE.removeGuest("030303-0303");
+		for(String id : IGuests.INSTANCE.getAllGuestIDs()){
+			IGuests.INSTANCE.removeGuest(id);
+		}
 		
 	}
 
@@ -44,107 +44,129 @@ public class IGuestsTest {
 
 	@Test
 	public void testAddGuest() {
-		fail("Not yet implemented");
+		IGuests.INSTANCE.addGuest("040404-0404", "Fatima", "Mohammed", "Mrs", "fatmima.mohammed@email.email", "0404040404");
+		List<String> guestsIDs = IGuests.INSTANCE.getAllGuestIDs();
+		assertTrue(guestsIDs.size() == 4);
 	}
 
 	@Test
 	public void testChangeGuestFirstName() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testChangeGuestLastName() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testChangeGuestTitle() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testChangeGuestEmail() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testChangeGuestPhone() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testGetGuestFirstName() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testGetGuestLastName() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testGetGuestTitle() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testGetGuestEmail() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testGetGuestPhone() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testSearchGuests() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testGetGuestStays() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testGetGuestRequests() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testRemoveGuestStay() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testAddGuestRequest() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testRemoveGuestRequest() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testGetGuestAccountUsername() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testGetGuestAccountPassword() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testGenerateGuestAccount() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 	@Test
 	public void testRemoveGuestAccount() {
 		fail("Not yet implemented");
+		//TODO: write it
 	}
 
 }
