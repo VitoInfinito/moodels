23c49e84a2cd23980fbbdb5c40a484a979cce821
LagerstedtMarcus <marlag@student.chalmers.se>
2015-01-03 11:29:17 +0100

    Started doing tests of the bookables

--- Hotel.tests/src/Classes/Bookables/tests/IBookablesManageTest.java
+++ Hotel.tests/src/Classes/Bookables/tests/IBookablesManageTest.java
@@ -2,33 +2,77 @@ package Classes.Bookables.tests;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import Classes.Bookables.ConferenceRoomCategory;
+import Classes.Bookables.HotelRoomCategory;
+import Classes.Bookables.IBookablesManage;
+import Classes.Bookings.IBookings;
+import Classes.Customers.ICustomers;
+import Classes.Stays.IStays;
+import Classes.Utils.InvalidIDException;
 
 public class IBookablesManageTest {
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		
 	}
 
 	@Before
 	public void setUp() throws Exception {
+		String bookableID = IBookablesManage.INSTANCE.addHotelRoom("001", 100, "desc for hotelroom 1", 1, "info", HotelRoomCategory.STANDARD_ROOM, 2);
+		List<String> bookableList1 = new ArrayList<String>();
+		bookableList1.add(bookableID);
+		bookableID = IBookablesManage.INSTANCE.addHotelRoom("002", 200, "desc for hotelroom 2", 1, "info", HotelRoomCategory.FAMILY_ROOM, 4);
+		bookableList1.add(bookableID);
+		bookableID = IBookablesManage.INSTANCE.addHotelRoom("003", 200, "desc for hotelroom 3", 1, "info", HotelRoomCategory.SUITE, 6);
+		bookableList1.add(bookableID);
+		bookableID = IBookablesManage.INSTANCE.addHotelRoom("004", 200, "desc for hotelroom 3", 1, "info", HotelRoomCategory.SUITE, 6);
+		bookableList1.add(bookableID);
+		bookableID = IBookablesManage.INSTANCE.addConferenceRoom("005", 1, "desc of conferenceroom", 3, "info", ConferenceRoomCategory.LECTURE_ROOM, 100);
+		bookableList1.add(bookableID);
+		bookableID = IBookablesManage.INSTANCE.addHotelRoom("006", 200, "desc for hotelroom with hostel bed", 1, "info", HotelRoomCategory.STANDARD_ROOM, 2);
+		bookableList1.add(bookableID);
+		bookableID = IBookablesManage.INSTANCE.addHostelBed("101", 50, "hostelbed", "006");
+		bookableList1.add(bookableID);
+	}
+	
+	@After
+	public void tearDown() throws InvalidIDException {
+		for(String id : IBookablesManage.INSTANCE.getAllBookableIDs()) {
+			IBookablesManage.INSTANCE.deleteBookable(id);
+		}
 	}
 
 	@Test
 	public void testChangeHostelBedRoom() {
-		fail("Not yet implemented");
+		IBookablesManage.INSTANCE.changeHostelBedRoom("101", "004");
+		boolean result = IBookablesManage.INSTANCE.getRoomOfHostelBed("101").equals("004");
+		assertTrue(result);
 	}
 
 	@Test
 	public void testDeleteBookable() {
-		fail("Not yet implemented");
+		IBookablesManage.INSTANCE.deleteBookable("001");
+		boolean result = IBookablesManage.INSTANCE.getAllBookableIDs().contains("001");
+		assertFalse(result);
 	}
 
 	@Test
 	public void testChangeRoomLocation() {
-		fail("Not yet implemented");
+		String tmp = "New Location";
+		IBookablesManage.INSTANCE.changeRoomLocation("002", 10, tmp);
+		boolean result = IBookablesManage.INSTANCE.getRoomLocationFloor("002") == 10 
+				&& IBookablesManage.INSTANCE.getRoomLocationInfo("002").equals(tmp);
+		assertTrue(result);
 	}
 
 	@Test
