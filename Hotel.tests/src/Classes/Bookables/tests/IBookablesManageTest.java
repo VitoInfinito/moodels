f39e1c6155dcb7c47a3f6f0cc22c5e3288219078
LagerstedtMarcus <marlag@student.chalmers.se>
2015-01-03 15:43:01 +0100

    Added some code to the tests and added some tests

--- Hotel.tests/src/Classes/Bookables/tests/IBookablesManageTest.java
+++ Hotel.tests/src/Classes/Bookables/tests/IBookablesManageTest.java
@@ -65,59 +65,101 @@ public class IBookablesManageTest {
 		boolean result = IBookablesManage.INSTANCE.getAllBookableIDs().contains("001");
 		assertFalse(result);
 	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testDeleteNoneExistingBookable() {
+		IBookablesManage.INSTANCE.deleteBookable("009");
+	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testChangeNoneExistingRoomLocation(){
+		IBookablesManage.INSTANCE.changeRoomLocation("009", 10, "");
+	}
 
 	@Test
-	public void testChangeRoomLocation() {
+	public void testChangeRoomLocationOnlyFloor() {
 		String tmp = "New Location";
 		IBookablesManage.INSTANCE.changeRoomLocation("002", 10, tmp);
-		boolean result = IBookablesManage.INSTANCE.getRoomLocationFloor("002") == 10 
-				&& IBookablesManage.INSTANCE.getRoomLocationInfo("002").equals(tmp);
+		boolean result = IBookablesManage.INSTANCE.getRoomLocationFloor("002") == 10 ;
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testChangeRoomLocationOnlyDesc() {
+		String tmp = "New Location";
+		IBookablesManage.INSTANCE.changeRoomLocation("002", 10, tmp);
+		boolean result = IBookablesManage.INSTANCE.getRoomLocationInfo("002").equals(tmp);
 		assertTrue(result);
 	}
 
 	@Test
 	public void testChangeHotelRoomCategory() {
-		fail("Not yet implemented");
+		String ID = "002";
+		boolean result = IBookablesManage.INSTANCE.getHotelRoomCategory(ID).equals(HotelRoomCategory.FAMILY_ROOM);
+		IBookablesManage.INSTANCE.changeHotelRoomCategory(ID, HotelRoomCategory.SUITE);
+		result = result && IBookablesManage.INSTANCE.getHotelRoomCategory(ID).equals(HotelRoomCategory.SUITE);
+		assertTrue(result);
 	}
 
 	@Test
 	public void testChangeConferenceRoomCategory() {
-		fail("Not yet implemented");
+		String ID = "005";
+		IBookablesManage.INSTANCE.changeConferenceRoomCategory(ID, ConferenceRoomCategory.DINING_ROOM);
+		boolean result = IBookablesManage.INSTANCE.getConferenceRoomCategory(ID).equals(ConferenceRoomCategory.DINING_ROOM);
+		assertTrue(result);
 	}
 
 	@Test
 	public void testChangeBookableBasePrice() {
-		fail("Not yet implemented");
+		String ID = "005";
+		IBookablesManage.INSTANCE.changeBookableBasePrice(ID, 1337);
+		boolean result = IBookablesManage.INSTANCE.getBookableBasePrice(ID) == 1337;
+		assertTrue(result);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testChangeBookableBasePriceToLessThanZero() {
+		String ID = "005";
+		IBookablesManage.INSTANCE.changeBookableBasePrice(ID, -1);
 	}
 
 	@Test
 	public void testChangeBookableDescription() {
-		fail("Not yet implemented");
+		String ID = "005";
+		String desc = "This is not bookable";
+		IBookablesManage.INSTANCE.changeBookableDescription(ID, desc);
+		boolean result = IBookablesManage.INSTANCE.getBookableDescription(ID).equals(desc);
+		assertTrue(result);
 	}
 
 	@Test
 	public void testAddHotelRoom() {
 		fail("Not yet implemented");
+		//TODO
 	}
 
 	@Test
 	public void testAddHostelBed() {
 		fail("Not yet implemented");
+		//TODO
 	}
 
 	@Test
 	public void testAddConferenceRoom() {
 		fail("Not yet implemented");
+		//TODO
 	}
 
 	@Test
 	public void testChangeHotelRoomNumberBeds() {
 		fail("Not yet implemented");
+		//TODO
 	}
 
 	@Test
 	public void testChangeConferenceRoomCapacity() {
 		fail("Not yet implemented");
+		//TODO
 	}
 
 }
