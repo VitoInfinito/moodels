4ac9b1702a4bc4327dc00634b509b66117d5c0a5
Daniel Bäckström <danbacks@student.chalmers.se>
2015-01-04 18:08:57 +0100

    Added full test suite

--- Hotel.tests/src/Classes/AllTests.java
+++ Hotel.tests/src/Classes/AllTests.java
@@ -9,9 +9,22 @@ import Classes.Accounts.tests.IManageAccountsTest;
 import Classes.Bills.tests.IBillsTest;
 import Classes.Bookables.tests.IBookablesAccessTest;
 import Classes.Bookables.tests.IBookablesManageTest;
+import Classes.Bookings.tests.IBookingsTest;
+import Classes.Customers.tests.ICustomersTest;
+import Classes.Feedback.tests.IFeedbackTest;
+import Classes.Guests.tests.IManageGuestsTest;
+import Classes.Inventory.tests.IInventoryAccessTest;
+import Classes.Inventory.tests.IManageInventoryTest;
+import Classes.Requests.tests.IRequestsTest;
+import Classes.Restaurants.tests.IRestaurantsAccessTest;
+import Classes.Restaurants.tests.IRestaurantsManageTest;
+import Classes.Services.tests.IServicesAccessTest;
+import Classes.Services.tests.IServicesManagerTest;
+import Classes.Staff.tests.IStaffTest;
+import Classes.Statistics.tests.IStatisticsGeneratorTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ IAccountsAccessTest.class, IManageAccountsTest.class, IBillsTest.class, IBookablesAccessTest.class, IBookablesManageTest.class })
+@SuiteClasses({ IAccountsAccessTest.class, IManageAccountsTest.class, IBillsTest.class, IBookablesAccessTest.class, IBookablesManageTest.class, IBookingsTest.class, ICustomersTest.class, IFeedbackTest.class, IManageGuestsTest.class, IInventoryAccessTest.class, IManageInventoryTest.class, IRequestsTest.class, IRestaurantsAccessTest.class, IRestaurantsManageTest.class, IServicesAccessTest.class, IServicesManagerTest.class, IStaffTest.class, IStatisticsGeneratorTest.class })
 public class AllTests {
 
 }
