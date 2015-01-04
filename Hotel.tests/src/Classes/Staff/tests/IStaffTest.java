9f98f37f9c1a4bbe8b09e849d36f02daecc873f9
Marcus Olsson <marcus@meeloo.se>
2015-01-04 10:09:04 +0100

    Added more tests

--- Hotel.tests/src/Classes/Staff/tests/IStaffTest.java
+++ Hotel.tests/src/Classes/Staff/tests/IStaffTest.java
@@ -8,7 +8,9 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import Classes.Feedback.IFeedback;
 import Classes.Staff.IStaff;
+import Classes.Utils.InvalidIDException;
 
 public class IStaffTest {
 	
@@ -52,35 +54,70 @@ public class IStaffTest {
 	public void testGetStaffFirstName() {		
 		assertTrue(IStaff.INSTANCE.getStaffFirstName("010101-0101") == "Sven");
 	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testGetStaffFirstName_id_not_exists() {
+		IStaff.INSTANCE.getStaffFirstName("");
+	}
 
 	@Test
 	public void testGetStaffLastName() {
 		assertTrue(IStaff.INSTANCE.getStaffLastName("010101-0101") == "Larsson");
+	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testGetStaffLastName_id_not_exists() {
+		IStaff.INSTANCE.getStaffLastName("");
 	}
 
 	@Test
 	public void testGetStaffJob() {
 		assertTrue(IStaff.INSTANCE.getStaffJob("010101-0101") == "Cleaner");
 	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testGetStaffJob_id_not_exists() {
+		IStaff.INSTANCE.getStaffJob("");
+	}
 
 	@Test
 	public void testGetStaffPhone() {
 		assertTrue(IStaff.INSTANCE.getStaffPhone("010101-0101") == "0705-232425");
+	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testGetStaffPhone_id_not_exists() {
+		IStaff.INSTANCE.getStaffPhone("");
 	}
 
 	@Test
 	public void testGetStaffEmail() {
 		assertTrue(IStaff.INSTANCE.getStaffEmail("010101-0101") == "sven.larsson@mail.com");
 	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testGetStaffEmail_id_not_exists() {
+		IStaff.INSTANCE.getStaffEmail("");
+	}
 
 	@Test
 	public void testGetStaffSalaryContractType() {
 		assertTrue(IStaff.INSTANCE.getStaffSalaryContractType("010101-0101") == "Hourly");
 	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testGetStaffSalaryContractType_id_not_exists() {
+		IStaff.INSTANCE.getStaffSalaryContractType("");
+	}
 
 	@Test
 	public void testGetStaffSalary() {
 		assertTrue(IStaff.INSTANCE.getStaffSalary("010101-0101") == 100);
+	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testGetStaffSalary_id_not_exists() {
+		IStaff.INSTANCE.getStaffSalary("");
 	}
 
 	@Test
@@ -88,11 +125,21 @@ public class IStaffTest {
 		IStaff.INSTANCE.changeStaffFirstName("010101-0101", "Kalle");
 		assertTrue(IStaff.INSTANCE.getStaffFirstName("010101-0101") == "Kalle");
 	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testChangeStaffFirstName_id_not_exists() {
+		IStaff.INSTANCE.changeStaffFirstName("", "");
+	}
 
 	@Test
 	public void testChangeStaffLastName() {
 		IStaff.INSTANCE.changeStaffLastName("010101-0101", "Andersson");
 		assertTrue(IStaff.INSTANCE.getStaffLastName("010101-0101") == "Andersson");
+	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testChangeStaffLastName_id_not_exists() {
+		IStaff.INSTANCE.changeStaffLastName("", "");
 	}
 
 	@Test
@@ -100,11 +147,21 @@ public class IStaffTest {
 		IStaff.INSTANCE.changeStaffJob("010101-0101", "Manager");
 		assertTrue(IStaff.INSTANCE.getStaffJob("010101-0101") == "Manager");
 	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testChangeStaffJob_id_not_exists() {
+		IStaff.INSTANCE.changeStaffJob("", "");
+	}
 
 	@Test
 	public void testChangeStaffPhone() {
 		IStaff.INSTANCE.changeStaffPhone("010101-0101", "0705-232427");
 		assertTrue(IStaff.INSTANCE.getStaffPhone("010101-0101") == "0705-232427");
+	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testChangeStaffPhone_id_not_exists() {
+		IStaff.INSTANCE.changeStaffPhone("", "");
 	}
 
 	@Test
@@ -123,6 +180,11 @@ public class IStaffTest {
 	public void testRemoveEmployee() {
 		IStaff.INSTANCE.removeStaff("010101-0101");
 		assertTrue(IStaff.INSTANCE.getAllStaff().size() == 1);
+	}
+	
+	@Test(expected=InvalidIDException.class)
+	public void testRemoveEmployee_id_not_exists() {
+		IStaff.INSTANCE.removeStaff("");
 	}
 
 	@Test
